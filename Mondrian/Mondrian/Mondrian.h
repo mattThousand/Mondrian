17e5e3cc74a85bca05a03223849f9d7e3240ee1a
mattthousand <matt.buckley@raizlabs.com>
2016-09-09 22:09:15 -0400

    Create Mondrian framework.

---
+++ Mondrian/Mondrian/Mondrian.h
@@ -0,0 +1,19 @@
+//
+//  Mondrian.h
+//  Mondrian
+//
+//  Created by Matthew Buckley on 9/9/16.
+//  Copyright © 2016 Nice Things. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mondrian.
+FOUNDATION_EXPORT double MondrianVersionNumber;
+
+//! Project version string for Mondrian.
+FOUNDATION_EXPORT const unsigned char MondrianVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mondrian/PublicHeader.h>
+
+
